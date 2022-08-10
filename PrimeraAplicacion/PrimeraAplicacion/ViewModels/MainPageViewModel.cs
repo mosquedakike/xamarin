075efb52b6906598e3c38576525650877bac02b0
Enrique Hernandez <entique_inf@hotmail.com>
2022-08-10 09:56:51 -0500

    primer commit

---
+++ PrimeraAplicacion/PrimeraAplicacion/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,38 @@
+﻿using PrimeraAplicacion.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace PrimeraAplicacion.ViewModels
+{
+    public class MainPageViewModel: NotificationObject
+    {
+
+        private ObservableCollection<Survey> survays;
+
+        public ObservableCollection<Survey> Survays
+        {
+            get { return survays; }
+            set 
+            { 
+                survays = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; set; }
+
+        public MainPageViewModel()
+        {
+            AddCommand = new Command(AddCommandExecute);
+        }
+
+        private void AddCommandExecute()
+        {
+            MessagingCenter.Send(this, "AddSurvey");
+        }
+    }
+}
---
+++ PrimeraAplicacion/PrimeraAplicacion/ViewModels/SurveyDetailsViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace PrimeraAplicacion.ViewModels
+{
+    public class SurveyDetailsViewModel: NotificationObject
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; OnPropertyChanged(); }
+        }
+
+        private string favoritefood;
+
+        public string FavoriteFood
+        {
+            get { return favoritefood; }
+            set { favoritefood = value; OnPropertyChanged(); }
+        }
+
+        public ICommand SaveCommand { get; set; }
+
+        public SurveyDetailsViewModel()
+        {
+            SaveCommand = new Command(() => 
+            { 
+                
+            });
+        }
+    }
+}
